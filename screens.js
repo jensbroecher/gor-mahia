2edab86030f5f7b23afb9fe3a1e718426c66a352
jensbroecher <jensbroecher@gmail.com>
2015-06-08 20:36:11 +0200

    go

--- screens.js
+++ screens.js
@@ -42,3 +42,28 @@ function register() {
 	document.getElementById('registeraccount').style.display = 'block';
 	document.getElementById("mainholder").className = "blur";
 }
+
+function registernext_prep_eng() {
+	var $myForm = $('#registernext_prep_eng')
+	if (!$myForm[0].checkValidity()) {
+    
+	}
+}
+function registernext2_prep_eng() {
+	var $myForm2 = $('#registernext2_prep_eng')
+	if (!$myForm2[0].checkValidity()) {
+   
+	}
+}
+function complete_eng_prep() {
+	var $myForm3 = $('#complete_eng')
+	if (!$myForm3[0].checkValidity()) {
+   
+	}
+}
+
+function maxLengthCheck(object)
+  {
+    if (object.value.length > object.maxLength)
+      object.value = object.value.slice(0, object.maxLength)
+  }
