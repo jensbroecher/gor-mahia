4ef35e3ee036be2cdccf3d17606660c317422615
jensbroecher <jensbroecher@gmail.com>
2015-06-09 09:38:16 +0200

    location

--- screens.js
+++ screens.js
@@ -4,6 +4,25 @@ document.getElementById("loadingindicator").style.display = "block";
 }
 
 $( document ).ready(function() {
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+	
+	localStorage.setItem('latitude', latitude);
+	localStorage.setItem('longitude', longitude);
+	
+  };
+
+  function error() {
+    
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+
+	
+	
 setTimeout(function(){
 	document.getElementById("loadingindicator").style.display = "none";
 	}, 1000);
@@ -43,6 +62,8 @@ function register() {
 	document.getElementById("mainholder").className = "blur";
 }
 function complete_eng() {
+var lat = localStorage.getItem('latitude');
+var long = localStorage.getItem('longitude');
 var pin_en = document.getElementById('pin_en').value;
 var city_en = document.getElementById('city_en').value;
 var gender_en = document.getElementById('gender_en').value;
@@ -51,9 +72,9 @@ var email_en = document.getElementById('email_en').value;
 var last_name_en = document.getElementById('last_name_en').value;
 var first_name_en = document.getElementById('first_name_en').value;
 
-alert('Data sent:\n\nfirst_name_en: '+first_name_en+'\nlast_name_en: '+last_name_en+'\nemail_en: '+email_en+'\nphone_en: '+phone_en+'\ngender_en: '+gender_en+'\ncity_en: '+city_en+'\npin_en: '+pin_en+'');
+alert('Data sent:\n\nfirst_name_en: '+first_name_en+'\nlast_name_en: '+last_name_en+'\nemail_en: '+email_en+'\nphone_en: '+phone_en+'\ngender_en: '+gender_en+'\ncity_en: '+city_en+'\npin_en: '+pin_en+'\nlong: '+long+'\nlat: '+lat+'');
 
-$.get('http://enunua.com/gormahia/fan_number.php?pin_en='+pin_en+'&city_en='+city_en+'&gender_en='+gender_en+'&phone_en='+phone_en+'&email_en='+email_en+'&last_name_en='+last_name_en+'&first_name_en='+first_name_en+'', function(data) {
+$.get('http://enunua.com/gormahia/fan_number.php?pin_en='+pin_en+'&city_en='+city_en+'&gender_en='+gender_en+'&phone_en='+phone_en+'&email_en='+email_en+'&last_name_en='+last_name_en+'&first_name_en='+first_name_en+'&lat='+lat+'&long='+long+'', function(data) {
   	alert('Sent');
 });
 
