a813f17f1a9523cb78b8e7fe4b63bfd4c840db42
jensbroecher <jensbroecher@gmail.com>
2015-06-23 09:26:59 +0200

    compile

--- screens.js
+++ screens.js
@@ -50,7 +50,6 @@ function registernext2() {
 	document.getElementById('register2').style.display = 'none';
 }
 function registerprev() {
-	document.getElementById('register3').style.display = 'none';
 	document.getElementById('register2').style.display = 'none';
 	document.getElementById('register1').style.display = 'block';
 }
@@ -62,9 +61,23 @@ function register() {
 	document.getElementById('registeraccount').style.display = 'block';
 	document.getElementById("mainholder").className = "blur";
 }
+function askhowmuch() {
+var theamount = prompt("How much would you like to contribute to the team?", "");
+
+if (theamount != null) {
+    localStorage.setItem('amount',theamount);
+	contribution();
+}
+}
+function contribution() {
+	document.getElementById('contribution').style.display = 'block';
+	complete_eng();
+}
+
 function complete_eng() {
 var lat = localStorage.getItem('latitude');
 var long = localStorage.getItem('longitude');
+var amount = localStorage.getItem('amount');
 var pin_en = document.getElementById('pin_en').value;
 var city_en = document.getElementById('city_en').value;
 var gender_en = document.getElementById('gender_en').value;
@@ -73,22 +86,10 @@ var email_en = document.getElementById('email_en').value;
 var last_name_en = document.getElementById('last_name_en').value;
 var first_name_en = document.getElementById('first_name_en').value;
 
-alert('Data sent:\n\nfirst_name_en: '+first_name_en+'\nlast_name_en: '+last_name_en+'\nemail_en: '+email_en+'\nphone_en: '+phone_en+'\ngender_en: '+gender_en+'\ncity_en: '+city_en+'\npin_en: '+pin_en+'\nlong: '+long+'\nlat: '+lat+'');
+alert('Data sent:\n\nfirst_name_en: '+first_name_en+'\nlast_name_en: '+last_name_en+'\nemail_en: '+email_en+'\nphone_en: '+phone_en+'\ngender_en: '+gender_en+'\ncity_en: '+city_en+'\npin_en: '+pin_en+'\nlong: '+long+'\nlat: '+lat+'\namount: '+amount+'');
 
-$.get('http://enunua.com/gormahia/fan_number.php?pin_en='+pin_en+'&city_en='+city_en+'&gender_en='+gender_en+'&phone_en='+phone_en+'&email_en='+email_en+'&last_name_en='+last_name_en+'&first_name_en='+first_name_en+'&lat='+lat+'&long='+long+'', function(data) {
-  	alert('Sent');
-});
+document.location.href='http://enunua.com/gormahia/fan_number.php?pin_en='+pin_en+'&city_en='+city_en+'&gender_en='+gender_en+'&phone_en='+phone_en+'&email_en='+email_en+'&last_name_en='+last_name_en+'&first_name_en='+first_name_en+'&lat='+lat+'&long='+long+'&amount='+amount+'';
 
-}
-
-function registernext_prep_eng() {
-	
-}
-function registernext2_prep_eng() {
-	
-}
-function complete_eng_prep() {
-	
 }
 
 function maxLengthCheck(object)
